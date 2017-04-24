ffbcc4dd33af1fdb38bb8acaf2599eb6b6a3b2f3
root <root@dave-fedora.domain>
2017-04-24 12:02:20 -0600

    Added ftoken endpoint

--- views.py
+++ views.py
@@ -16,10 +16,10 @@ from rest_framework.parsers import JSONParser
 # Create your views here.
 
 class RequestUser(APIView):
-
   def post(self, request, format=None):
     #Check if user exists
     data = JSONParser().parse(request)
+    print(data)
     name = data['user']['name']
     if User.objects.filter(name = name).exists():
       user = User.objects.get(name = name)
@@ -30,6 +30,7 @@ class RequestUser(APIView):
       bag = Bag.objects.create()
       bag.save()
       data['bag'] = bag.id
+      print(data)
       serializer = UserSerializer(data=data)
 
       if serializer.is_valid():
@@ -37,7 +38,34 @@ class RequestUser(APIView):
         return Response({"user" : serializer.data}, status=status.HTTP_201_CREATED)
       else:
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-     
 
-  
+
+class SubmitFcmToken(APIView):
+  def post(self, request, format=None):
+    data = JSONParser().parse(request)
+    u_id = data['user']['id']
+    token_body = data['FcmToken']['body']
+
+    if u_id and token_body and User.objects.filer(id = u_id).exists():
+      token_dict = {
+        'FcmToken': {
+          'body': token_body,
+          'user': u_id
+        }
+      }
+      serializer = FcmTokenSerializer(data=token_dict['FcmToken'])
+      if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+      else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    else:
+      return Response("Invalid User", status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
+
 
